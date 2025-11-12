3e516f5212e8d9df8ef8973f3281272c71c13ec3
mykhailopulov <mihail.pulov.247@gmail.com>
2025-11-12 10:41:16 +0200

    1

--- kurwa/src/Breakout.java
+++ kurwa/src/Breakout.java
@@ -7,13 +7,7 @@
  * This file will eventually implement the game of Breakout.
  */
 
-import acm.graphics.*;
 import acm.program.*;
-import acm.util.*;
-
-import java.applet.*;
-import java.awt.*;
-import java.awt.event.*;
 
 public class Main extends GraphicsProgram {
 
