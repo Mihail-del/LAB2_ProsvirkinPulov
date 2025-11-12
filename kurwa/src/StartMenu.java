d95821f493768bbe224ba1333a109ad1d8158941
mykhailopulov <mihail.pulov.247@gmail.com>
2025-11-12 16:54:05 +0200

    settings Class StartMenu

--- kurwa/src/StartMenu.java
+++ kurwa/src/StartMenu.java
@@ -9,31 +9,57 @@
  */
 
 import acm.graphics.GImage;
+import acm.graphics.GLabel;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class StartMenu extends GraphicsProgram {
     // StartMenu(){}
 
+    /** ===== LOCAL CONSTANTS ====== */
+    private static final int SETTING_WIDTH = 300;
+    private static final int SETTING_PADDING = 30;
+
+    private static final int WIDTH = Breakout.APPLICATION_WIDTH + SETTING_WIDTH + 2*SETTING_PADDING;
+    private static final int HEIGHT = Breakout.APPLICATION_HEIGHT + 2*SETTING_PADDING;
 
     // drawing the menu
     public void run(){
         setBackground(Breakout.bgColor);
-        setSize(Breakout.APPLICATION_WIDTH, Breakout.APPLICATION_HEIGHT);
-        setSize(2*(Breakout.APPLICATION_WIDTH)-getWidth(), 2*(Breakout.APPLICATION_HEIGHT)-getHeight());
+        setSize(WIDTH, HEIGHT);
+        setSize(2*WIDTH-getWidth() + 300, 2*HEIGHT-getHeight());
 
         header();
         paddleSettings();
+
+        preview();
     }
 
     /** ===== HEADER ===== */
     private void header(){
-        GImage image = new GImage("../images/gameSettings.png");
-        image.setSize(Breakout.APPLICATION_WIDTH*0.5, Breakout.APPLICATION_WIDTH*9/16.0);
+        GImage image = new GImage("images/gameSettings.png");
+        double imageWidth = WIDTH*0.8;
+        image.setSize(imageWidth, imageWidth*0.1);
+        image.setLocation((WIDTH-imageWidth)/2,HEIGHT*0.05);
         add(image);
     }
 
     private void paddleSettings(){
+        GLabel paddleLbl = new GLabel("Paddle Width");
+
+    }
+
+
+
+
+
+
+
+    private void preview(){
+        GRect gameFramePreviewer = new GRect(SETTING_WIDTH+ SETTING_PADDING, SETTING_PADDING, Breakout.APPLICATION_WIDTH, Breakout.APPLICATION_HEIGHT);
+        gameFramePreviewer.setFilled(true);
+        gameFramePreviewer.setFillColor(Breakout.bgColor);
+        add(gameFramePreviewer);
 
     }
 }
