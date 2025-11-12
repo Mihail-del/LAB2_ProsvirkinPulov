56861f525a2276b43f86ac18f0cf79aa7cad7653
Veronika <veronika.prosvirkin@gmail.com>
2025-11-12 10:31:54 +0200

    1

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("Tovariszi");
         System.out.println("Tovariszi");
         System.out.println("Tovariszi");
+        System.out.println("Gde ya?");
     }
 }
